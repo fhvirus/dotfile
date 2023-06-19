2b5611748e86c93454198e6642d7b8c59b166bef
FHVirus <froghackervirus@gmail.com>
2023-06-19 13:15:56 +0800

    fcitx5

--- config/nvim/lua/first-setup/options.lua
+++ config/nvim/lua/first-setup/options.lua
@@ -32,3 +32,27 @@ end)
 vim.keymap.set("n", "<leader>z", function()
     vim.cmd("ZenMode")
 end)
+
+-- Modified from https://zhuanlan.zhihu.com/p/558359369
+vim.cmd [[
+let g:input_toggle = 1
+function! Fcitx2normal()
+   let s:input_status = system("fcitx5-remote")
+   if s:input_status == 2
+      let g:input_toggle = 1
+      let l:a = system("fcitx5-remote -c")
+   endif
+endfunction
+
+function! Fcitx2insert()
+   let s:input_status = system("fcitx5-remote")
+   if s:input_status != 2 && g:input_toggle == 1
+      let l:a = system("fcitx5-remote -o")
+      let g:input_toggle = 0
+   endif
+endfunction
+
+set ttimeoutlen=150
+autocmd InsertLeave * call Fcitx2normal()
+autocmd InsertEnter * call Fcitx2insert()
+]]
