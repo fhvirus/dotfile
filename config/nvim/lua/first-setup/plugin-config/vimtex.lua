11d1990dbb2d6cf478f8aae8f48a0f8d375c5548
FHVirus <froghackervirus@gmail.com>
2023-09-07 10:05:07 +0800

    Update

---
+++ config/nvim/lua/first-setup/plugin-config/vimtex.lua
@@ -0,0 +1,21 @@
+vim.cmd [[
+let g:tex_flavor='latex'
+let g:vimtex_view_method='zathura'
+let g:vimtex_quickfix_mode=0
+let g:vimtex_compiler_method = 'latexmk'
+let g:vimtex_compiler_latexmk = {
+  \ 'build_dir' : '',
+  \ 'callback' : 1,
+  \ 'continuous' : 1,
+  \ 'executable' : 'latexmk',
+  \ 'hooks' : [],
+  \ 'options' : [
+  \   '-pdfxe',
+  \   '-shell-escape',
+  \   '-verbose',
+  \   '-file-line-error',
+  \   '-synctex=1',
+  \   '-interaction=nonstopmode',
+  \ ],
+  \}
+]]
