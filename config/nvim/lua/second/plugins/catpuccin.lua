adf9db406b4a0e3d41bb9225b994237ca124ad5e
FHVirus <froghackervirus@gmail.com>
2024-01-16 08:09:34 +0800

    Update nvim one last time (maybe)

--- config/nvim/lua/second/plugins/catpuccin.lua
+++ config/nvim/lua/second/plugins/catpuccin.lua
@@ -1,12 +1,12 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  enabled = false,
-  lazy = true,
+  -- enabled = false,
+  -- lazy = true,
   priority = 1000,
-  init = function()
-    vim.cmd.colorscheme "catppuccin"
-  end,
+  -- init = function()
+  --   vim.cmd.colorscheme "catppuccin"
+  -- end,
   opts = {
     flavour = "frappe",
   }
