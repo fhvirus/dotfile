a4402f64fd08e9b6aeb8bd16b6daec75ee651066
FHVirus <froghackervirus@gmail.com>
2025-11-02 18:58:02 +0100

    feat(nvim): added LSP completion & completeopt

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,17 @@ vim.lsp.config("tinymist", {
   }
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf)
+    end
+  end,
+})
+vim.opt.completeopt:append({ "menuone", "noselect", "popup", "longest" })
+
 return {
   {
     "mason-org/mason-lspconfig.nvim",
